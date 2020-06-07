8f20a34823e9555fcbc17ac3367f8064774bd1cd
João Paulo Sena <joaopaulo761@gmail.com>
2020-06-07 16:22:25 -0300

    :rocket: Release 0.0.1

--- app/src/main/java/dev/forcetower/instrack/arch/FirebaseMessageService.kt
+++ app/src/main/java/dev/forcetower/instrack/arch/FirebaseMessageService.kt
@@ -6,6 +6,6 @@ import timber.log.Timber
 class FirebaseMessageService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        Timber.d("New token!")
+        Timber.d("New token! $token")
     }
 }
